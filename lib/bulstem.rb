3a74ad01c26739ce883bf9e5304e8eafe492892a
Stanislav Gatev <s2gatev@gmail.com>
2013-01-29 00:07:13 +0200

    Refactored the code.

--- lib/bulstem.rb
+++ lib/bulstem.rb
@@ -1,28 +1,26 @@
-﻿class BulStem
+class BulStem
+  VOCALS = /[^аъоуеияю]*[аъоуеияю]/
+  RULE_PATTERN = /([а-я]+)\s==>\s([а-я]+)\s([0-9]+)/
+
   @@boundary = 1
-  @@vocals = /[^аъоуеияю]*[аъоуеияю]/
-  @@pattern = /([а-я]+)\s==>\s([а-я]+)\s([0-9]+)/
 
   def initialize(file_name)
-    @rules = {}
     load_rules file_name
   end
 
   def load_rules(file_name)
-    open file_name, 'r:UTF-8' do |file|
+    @rules = {}
+    File.open file_name, 'r:UTF-8' do |file|
       while line = file.gets
-          match = line.match @@pattern
-          @rules[match[1]] = match[2] if match and match.length > 3 and match[3].to_i > @@boundary
+        @rules[$1] = $2 if line.chomp.match RULE_PATTERN and $3.to_i > @@boundary
       end
     end
   end
 
   def stem(word)
-    match = word.match @@vocals
+    match = word.match VOCALS
     return word unless match and word.length > 3
-    (3..word.length).each do |i|
-      return word[0...i] + @rules[word[i..-1]] if @rules.include? word[i..-1]
-    end
+    (3..word.length).each { |i| return word[0...i] + @rules[word[i..-1]] if @rules.include? word[i..-1] }
     word
   end
 end
