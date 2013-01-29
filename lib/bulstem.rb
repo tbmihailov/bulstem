6baa8d724e9c09b4557a3570d6f3693cfe68b277
Stanislav Gatev <s2gatev@gmail.com>
2013-01-29 11:40:12 +0200

    Abstracted the contexts as levels

--- lib/bulstem.rb
+++ lib/bulstem.rb
@@ -1,20 +1,29 @@
 class BulStem
+  class Level
+    def self.low
+      1
+    end
+
+    def self.medium
+      2
+    end
+
+    def self.high
+      3
+    end
+  end
+
   VOCALS = /[^аъоуеияю]*[аъоуеияю]/
   RULE_PATTERN = /([а-я]+)\s==>\s([а-я]+)\s([0-9]+)/
 
   @@boundary = 1
 
-  def initialize(file_name)
-    load_rules file_name
+  def initialize(level=Level.low)
+    load_rules level
   end
 
-  def load_rules(file_name)
-    @rules = {}
-    File.open file_name, 'r:UTF-8' do |file|
-      while line = file.gets
-        @rules[$1] = $2 if line.chomp.match RULE_PATTERN and $3.to_i > @@boundary
-      end
-    end
+  def set_level(level)
+    load_rules level
   end
 
   def stem(word)
@@ -22,5 +31,16 @@ class BulStem
     return word unless match and word.length > 3
     (3..word.length).each { |i| return word[0...i] + @rules[word[i..-1]] if @rules.include? word[i..-1] }
     word
+  end
+
+  private
+
+  def load_rules(level)
+    @rules = {}
+    File.open "../rules/stem_rules_context_#{level}.txt", 'r:UTF-8' do |file|
+      while line = file.gets
+        @rules[$1] = $2 if line.chomp.match RULE_PATTERN and $3.to_i > @@boundary
+      end
+    end
   end
 end
